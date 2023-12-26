8be636cf4a03064a63fe358329cf2590870cf81a
Leonardo Cunico <leonardo@cunico.net>
2023-12-26 14:27:19 +0100

    optimizations and fixes

--- src/three_wrapper/MeshLink.ts
+++ src/three_wrapper/MeshLink.ts
@@ -1,40 +1,43 @@
-import { CylinderGeometry, Mesh, MeshStandardMaterial, Vector3 } from "three";
+import { CylinderGeometry, Mesh, MeshStandardMaterial, Vector3, MathUtils } from "three";
 import { distanceTo } from "./Geometry";
 
 class MeshLink {
-    point1 = new Vector3(0, 0, 0);
-    point2 = new Vector3(0, 0, 0);
-    mesh: Mesh;
+	point1 = new Vector3(0, 0, 0);
+	point2 = new Vector3(0, 0, 0);
+	mesh: Mesh;
 
-    constructor(point1: Vector3, point2: Vector3) {
-        this.point1 = point1;
-        this.point2 = point2;
-        this.mesh = this.build();
-    }
+	constructor(point1: Vector3, point2: Vector3) {
+		this.point1 = point1;
+		this.point2 = point2;
+		this.mesh = this.build();
+	}
 
-    build(): Mesh {
-        // create cylinder between two points
-        const geometry = new CylinderGeometry(0.1, 0.1, 1, 32);
-        const material = new MeshStandardMaterial({ color: "gray" });
-        const cylinder = new Mesh(geometry, material);
+	build(): Mesh {
+		// create cylinder between two points
+		const size = 0.01;
+		const geometry = new CylinderGeometry(size, size, 1, 16);
+		geometry.rotateX(MathUtils.degToRad(90));
+		const material = new MeshStandardMaterial({ color: "blue" });
+		const cylinder = new Mesh(geometry, material);
 
-        cylinder.position.copy(this.point1);
-        cylinder.position.lerp(this.point2, 0.5);
-        cylinder.lookAt(this.point1);
-        cylinder.scale.set(1, 1, distanceTo(this.point1, this.point2));
+		this.updatePositionRotationScale(cylinder, this.point1, this.point2);
 
-        cylinder.visible = true;
-        return cylinder;
-    }
+		cylinder.visible = true;
+		return cylinder;
+	}
 
-    update(point1: Vector3, point2: Vector3): void {
-        this.point1 = point1;
-        this.point2 = point2;
-        this.mesh.position.copy(this.point1);
-        this.mesh.position.lerp(this.point2, 0.5);
-        this.mesh.lookAt(this.point1);
-        this.mesh.scale.set(1, 1, distanceTo(this.point1, this.point2));
-    }
+	update(point1: Vector3, point2: Vector3): void {
+		this.point1 = point1;
+		this.point2 = point2;
+		this.updatePositionRotationScale(this.mesh, this.point1, this.point2);
+	}
+
+	private updatePositionRotationScale(mesh: Mesh, point1: Vector3, point2: Vector3) {
+		mesh.position.copy(point1);
+		mesh.position.lerp(point2, 0.5);
+		mesh.lookAt(point1);
+		mesh.scale.set(1, 1, distanceTo(point1, point2));
+	}
 }
 
 export { MeshLink }
