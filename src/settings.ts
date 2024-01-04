7ef593e369ebaf17ad7d62210b4e00850a8e8450
Federico Cunico <federico@cunico.net>
2024-01-04 20:23:28 +0100

    added port from server

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,7 @@
+declare const SERVER_PORT_FROM_SERVER: string | undefined  // exists in index.html as a global variable
+
 const SERVER_IP = "127.0.0.1"
-const SERVER_PORT = 11000
+const SERVER_PORT = (SERVER_PORT_FROM_SERVER as any) || 40000
 const DATA_API = "data"
 
 export { SERVER_IP, SERVER_PORT, DATA_API }
